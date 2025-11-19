c5c5d8308f88b3e66000de6d714cc3336f9d803c
cludley <cludley@anamosa.k12.ia.us>
2025-11-19 16:58:46 +0000

    stuff

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+var school = {
+	name: "Knights Academy",
+	"average class size": 25,
+	mascot: "Knight"
+};
 
+document.getElementById('result').innerHTML = `${school.name} has an average class size of ${school['average class size']} and their mascot is a ${school.mascot}`;
